2d63df136deecd36550dddfdf3f0b24414df191f
Rory Preddy <rorypreddy@gmail.com>
2025-03-03 07:44:29 +0200

    Updates

---
+++ _next/static/3BZx0u5HB32qLqaKtt0k3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-07f6ebbec17cfed6.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
